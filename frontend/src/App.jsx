758e278b388e0119675f86cf33c4005c74470e90
Daniel Molina <danielmolina9202@gmail.com>
2024-09-19 19:22:10 -0300

    Ruta protegida implementada importando el valor del token en el app.jsx y cumpliendo la funcion de que si es false, redirige a login. Si es true no pueden acceder a la pagina de login y register, también se implementa cuando se le da al boton logout que redireccione al Home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,19 +9,25 @@ import  Cart from "./Pages/Cart";
 import { Pizza } from "./Pages/Pizza";
 import { Profile } from "./Pages/Profile";
 import { NotFound } from "./Pages/NotFound";
+import { UserContext } from "./Context/UserContext";
+import { useContext } from "react";
 
 function App() {
+
+  const { token } = useContext(UserContext);	
+
   return (
     <>
       <div className="min-vh-100 bg-dark">
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={token ? <Home /> : <Login />} />
+          <Route path="/logout" element={<Home />} />
+          <Route path="/register" element={token ? <Home /> : <Register />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/pizzas/:id" element={<Pizza />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route path="/profile" element={token ? <Profile /> : <Login />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
         <Footer />
